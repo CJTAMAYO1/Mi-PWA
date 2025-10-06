0cd2c209d9228a57556f7cd8f0a264127dd2d2be
Carlo Tamayo <cejota708@gmail.com>
2025-10-06 11:48:51 -0600

    MI PWA

---
+++ sw.js
@@ -0,0 +1,65 @@
+// Plantilla de service worker
+
+//1. nombre del cache y archivos a cachear
+
+const CACHE_NAME = "Nombre-del-cache"
+
+const urlsToCache = [ 
+    "index.html",
+    "styles.css",
+    "app.js",
+    "offline.html"
+]
+
+/*  2.IINSTALL => evento que se ejecuta al installa el service worker
+    Se dispara la primera vez que se registra el service worker
+*/
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache.addAll(urlsToCache))
+    ); 
+});
+
+// 3. ACTIVATE -> este evento se ecjecuta al activarse (debe limpiar cahes viejas)
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then( keys =>
+            Promise.all(
+                keys.filter( key => key !== CACHE_NAME )
+                .map( key => caches.delete(key)
+            )
+        )
+    )
+    );
+});
+
+ // 4. FETCH -> intercepta las peticiones de la PWA 
+ // Intercepta cada peticion de cada pagina de la PWA
+// Busca Primero en cache y si no lo encuentra va a la red
+// si falla todo, muestra una pagina offline.html
+ self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then( response => {
+            return response || fetch(event.request).catch(
+                () => caches.match("offline.html"))
+        })
+    );
+ });
+
+
+ // 5. Push -> Notificaciones en segundo plano (Opcional)
+ 
+ self.addEventListener("push", event => {
+    const data = event.data ? event.data.text() : "Notificación sin datos";
+    event.waitUntil(
+        self.registration.showNotification("Mi PWA", { body : data })
+    );
+ });
+
+ // Opcional:
+//          Sync -> Sincronización en segundo plano (Opcional)
+//          Maneja eventos de API's que el navegador soporte
+
+
